c6f423a490c74eda1f20bb08d02ef73828168ab5
lng2020 <nanguanlin6@gmail.com>
2022-12-01 22:52:57 +0800

    first commit in compiler

---
+++ code/code.go
@@ -0,0 +1,58 @@
+package code
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+const (
+	OpConstant Opcode = iota
+)
+
+var definition = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+type Instruction []byte
+
+type Opcode byte
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definition[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opconde %d undefined", op)
+	}
+
+	return def, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definition[op]
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLen := 1
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+	return instruction
+}
