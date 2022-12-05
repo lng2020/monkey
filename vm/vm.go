1913dab0d8de510e19d9a36de582640179ef43c8
lng2020 <nanguanlin6@gmail.com>
2022-12-05 22:39:42 +0800

    finish pop function

--- vm/vm.go
+++ vm/vm.go
@@ -47,6 +47,8 @@ func (vm *VM) Run() error {
 
 			result := leftValue + rightValue
 			vm.push(&object.Integer{Value: result})
+		case code.OpPop:
+			vm.pop()
 		}
 	}
 	return nil
@@ -74,4 +76,8 @@ func (vm *VM) pop() object.Object {
 	o := vm.stack[vm.sp-1]
 	vm.sp--
 	return o
+}
+
+func (vm *VM) LastPoppedStackElem() object.Object {
+	return vm.stack[vm.sp]
 }
