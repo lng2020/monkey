4a3e4b0044dc069129a1680994920be6d4677281
lng2020 <nanguanlin6@gmail.com>
2023-02-24 18:54:26 +0800

    implementing local bindings in the vm

--- object/object.go
+++ object/object.go
@@ -182,6 +182,7 @@ type Hashable interface {
 
 type CompiledFunction struct {
 	Instructions code.Instructions
+	NumLocals    int
 }
 
 func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
