0b1e939025a307fbabf92d32f5c0b20c6f021ea1
lng2020 <nanguanlin6@gmail.com>
2023-03-01 22:25:06 +0800

    executing built-in functions

--- object/builtins.go
+++ object/builtins.go
@@ -57,7 +57,7 @@ var Builtins = []struct {
 					return newError("wrong number of arguments. got=%d, want=1", len(args))
 				}
 				if args[0].Type() != ARRAY_OBJ {
-					return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+					return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
 				}
 
 				arr := args[0].(*Array)
@@ -77,7 +77,7 @@ var Builtins = []struct {
 					return newError("wrong number of arguments. got=%d, want=1", len(args))
 				}
 				if args[0].Type() != ARRAY_OBJ {
-					return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+					return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
 				}
 
 				arr := args[0].(*Array)
@@ -99,7 +99,7 @@ var Builtins = []struct {
 					return newError("wrong number of arguments. got=%d, want=2", len(args))
 				}
 				if args[0].Type() != ARRAY_OBJ {
-					return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+					return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
 				}
 
 				arr := args[0].(*Array)
